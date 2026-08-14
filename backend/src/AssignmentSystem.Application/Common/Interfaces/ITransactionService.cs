8ecfe8c39db1f575fb5676fcba56542ab46b307e
MdHasibAskari <mdhsbaskari@gmail.com>
2026-08-15 00:02:31 +0600

    chore(infrastructure): remove obsolete transaction service
    - ITransactionService and TransactionService are no longer referenced; the unit of work owns persistence flushes now

--- backend/src/AssignmentSystem.Application/Common/Interfaces/ITransactionService.cs
+++
@@ -1,6 +0,0 @@
-namespace AssignmentSystem.Application.Common.Interfaces;
-
-public interface ITransactionService
-{
-    Task ExecuteAsync(Func<CancellationToken, Task> work, CancellationToken ct = default);
-}
